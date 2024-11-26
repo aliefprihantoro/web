c5338dea9b7f2e993bdf280e06c9deabe3b9e4f8
aliefprihantoro <aliefprihantoro@gmail.com>
2024-11-26 20:42:08 +0700

    update data

--- configs/data/home/listContact.ts
+++ configs/data/home/listContact.ts
@@ -1,9 +1,9 @@
 export default [
-  ['email', 'alifprihantoro@gmail.com', '/icons/sosmed/email.svg'],
-  ['github', 'alifprihantoro', '/icons/sosmed/github.svg'],
-  ['twitter', '@alifprihantoro', '/icons/sosmed/twitter.svg'],
-  ['youtube', '@alifprihantoro', '/icons/sosmed/youtube.svg'],
+  ['email', 'aliefprihantoro@gmail.com', '/icons/sosmed/email.svg'],
+  ['github', 'aliefprihantoro', '/icons/sosmed/github.svg'],
+  ['twitter', '@aliefprihantoro', '/icons/sosmed/twitter.svg'],
+  ['youtube', '@aliefprihantoro', '/icons/sosmed/youtube.svg'],
   ['discord', 'discord', '/icons/sosmed/discord.svg'],
   ['whatsapp', '+62 83829383123', '/icons/sosmed/whatsapp.svg'],
-  ['telegram', '@alifprihantoro', '/icons/sosmed/telegram.svg'],
+  ['telegram', '@aliefprihantoro', '/icons/sosmed/telegram.svg'],
 ]
